d41d4b76daa8438e258c2d1c3c44991c41cb1e6e
kfirkfir89 <kfirkfir89@gmail.com>
2023-08-09 20:37:24 +0300

    setup backend , webscripting of linked in job url done

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 1,
+    '@typescript-eslint/no-unused-vars': 1,
+    'linebreak-style': 0,
+    '@typescript-eslint/no-redeclare': 0,
+    'import/prefer-default-export': 0,
+    'no-underscore-dangle': 0,
+    '@typescript-eslint/naming-convention': 0,
+    'max-len': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  root: true,
+  env: {
+    jest: true,
+  },
+  ignorePatterns: ['package.json'],
+};
