ee9cedbc3f74006914689052afe3444e2c418fba
kfirkfir89 <kfirkfir89@gmail.com>
2023-10-29 21:07:33 +0200

    client uploadFile component created for uploading user cv

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import UploadFile from './components/upload-file/uploadFile.component';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <span className="text-2xl  bg-red-900 font-bold ">hello</span>
+      <UploadFile />
     </div>
   );
 }
---
+++ client/src/components/upload-file/uploadFile.component.tsx
@@ -0,0 +1,12 @@
+
+const UploadFile = () => {
+  return (
+    <div className="">
+      <input type="file" className="file-input file-input-bordered file-input-success w-full max-w-xs" />
+
+      <button className="btn-success btn">Upload</button>
+    </div>
+  );
+}
+
+export default UploadFile;
