446fe8fc59fd02748e656b08293895d39714a594
kfirkfir89 <kfirkfir89@gmail.com>
2024-01-08 20:44:34 +0200

    two components created - errorAlert , generateButton

--- client/src/components/generate-cover-letter/generateCoverLetter.component.tsx
+++ client/src/components/generate-cover-letter/generateCoverLetter.component.tsx
@@ -1,6 +1,8 @@
 import LinkInput from "../link-input/linkInput.component";
 import UploadFile from "../upload-file/uploadFile.component";
 import useCoverLetterGenerator from '../../hooks/useCoverLetterGenerator';
+import ErrorAlert from "../error-alert/errorAlert.component";
+import GenerateButton from "../generate-button/generateButton.component";
 
 const Instructions = () => {
   return (
@@ -23,21 +25,6 @@ const Title = () => {
     </div>
   )
 }
-const Error = ({ error }: { error: string }) => {
-  return (
-    <>
-      {
-        error && (
-          <div role="alert" className="alert alert-warning">
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-            <span>Warning: {error}</span>
-          </div>
-        )
-      }
-    </>
-
-  )
-}
 const Footer = () => {
   return (
     <div className="flex flex-col w-full justify-end h-full">
@@ -56,7 +43,7 @@ const GenerateCoverLetter = () => {
     <div className="h-full w-full bg-slate-50 p-8">
       <div className="flex flex-col w-full h-full gap-4 items-center">
         <Title />
-        <Error error={error} />
+        <ErrorAlert error={error} />
 
         <div className="grid grid-cols-1 md:grid-cols-5 w-full gap-10 max-w-4xl">
           <div className="md:col-span-5 ">
@@ -68,24 +55,11 @@ const GenerateCoverLetter = () => {
           </div>
 
           <div className="md:col-span-2 flex justify-center w-full">
-            <button disabled={!(url && file)} onClick={handleSubmit} className="btn btn-ghost rounded-xl h-full w-full text-sky-600 bg-yellow-200 hover:text-yellow-200">
-              <span className="w-full flex flex-col items-center justify-center">
-                {isLoading ?
-                  <span className="loading loading-ring text-sky-100 w-1/2"></span>
-                  :
-                  <span className="relative z-20 flex items-center text-lg">
-                    Generate
-                    <svg className="relative w-5 h-5 ml-2 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                    </svg>
-                  </span>
-                }
-              </span>
-            </button>
+            <GenerateButton disabled={!(url && file)} onClick={handleSubmit} isLoading={isLoading} />
           </div>
         </div>
         {blobUrl && (
-          <a className="btn-info w-full max-w-sm my-4 btn" href={blobUrl} download={downloadFileName}>
+          <a className="btn btn-primary rounded-xl w-full py-5 pb-8 my-4" href={blobUrl} download={downloadFileName}>
             Download Cover Letter
           </a>
         )}
